adae1fd10f13f29f21f2d9cdbbf78a008dfe3764
Hemank <hemank.mittal@techextensor.com>
2024-12-31 13:10:55 +0530

    new class for global parameters, that can be used as parameters for all the test suites.

---
+++ BudgetItemAutomationIFM/globalParameters.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by Ranorex
+ * User: LENOVO
+ * Date: 30-12-2024
+ * Time: 16:19
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+
+namespace BudgetItemAutomationIFM
+{
+	/// <summary>
+	/// Description of globalParameters.
+	/// </summary>
+	public class globalParameters
+	{
+		public static string browserName = "Chrome";
+	}
+}
